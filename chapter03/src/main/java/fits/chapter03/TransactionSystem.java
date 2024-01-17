f90c7cfcb62a79fcc0798fe01b9c8260922f2d1f
Owolabi Legunsen <legunsen@cornell.edu>
2024-01-17 14:23:38 -0500

    added mvnized version of chapter 3

---
+++ chapter03/src/main/java/fits/chapter03/TransactionSystem.java
@@ -0,0 +1,24 @@
+package fits.chapter03;
+
+public class TransactionSystem {
+
+	private FrontEnd frontend;
+	private BackEnd backend;
+
+	public TransactionSystem() {
+		setup();
+	}
+
+	public FrontEnd getFrontEnd() {
+		return frontend;
+	}
+
+	public BackEnd getBackEnd() {
+		return backend;
+	}
+
+	public void setup() {
+		backend = new BackEnd();
+		frontend = new FrontEnd(backend);
+	}
+}
