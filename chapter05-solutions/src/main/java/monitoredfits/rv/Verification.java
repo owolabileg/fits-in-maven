43dd19178726608233b488a41ee3404f85233460
Owolabi Legunsen <legunsen@cornell.edu>
2024-01-19 15:41:29 -0500

    copy over chapter05 to chapter05solutions

---
+++ chapter05-solutions/src/main/java/monitoredfits/rv/Verification.java
@@ -0,0 +1,28 @@
+package monitoredfits.rv;
+
+import monitoredfits.assertion.Assertion;
+
+public class Verification {
+
+	private Verification() {
+		setupVerification();
+	}
+
+	private static Boolean fitsHasBeenInitialised = false;
+
+	// Called to start verification
+	public static void setupVerification() {
+		fitsHasBeenInitialised = false;
+	}
+
+	// Called from TransactionSystem.initialise
+	public static void fitsInitialisation() {
+		fitsHasBeenInitialised = true;
+	}
+
+	// Called from UserInfo.openSession
+	public static void fitsOpenSession() {
+		Assertion.check(fitsHasBeenInitialised, "Property 2 violated");
+	}
+
+}
