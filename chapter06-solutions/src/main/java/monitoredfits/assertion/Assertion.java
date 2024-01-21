2cac043db22b267cd5fc2ad8d93918463e50b9be
Owolabi Legunsen <legunsen@cornell.edu>
2024-01-20 23:41:12 -0500

    chapter06-solutions

---
+++ chapter06-solutions/src/main/java/monitoredfits/assertion/Assertion.java
@@ -0,0 +1,14 @@
+package monitoredfits.assertion;
+
+public class Assertion {
+
+	public static void alert(String errorString) {
+		System.out.println(errorString);
+	}
+
+	public static void check(Boolean condition, String errorString) {
+		if (!condition)
+			alert(errorString);
+	}
+
+}
