03e03709b75d39a72a66e769e1dfe5be6a8ba04a
Owolabi Legunsen <legunsen@cornell.edu>
2024-01-20 19:39:06 -0500

    [WIP] mavenizing in progress

--- chapter06/src/main/java/codegen/egcl/Main.java
+++ chapter06/src/main/java/codegen/egcl/Main.java
@@ -4,21 +4,23 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println(Arrays.asList(args));
 		try {
-			String path = "/home/owolabi/projects/fits-in-maven/chapter06/";
-			EGCLScript script = new EGCLScript(path + "src/main/resources/properties.egcl");
+			String path = args[0];
+			EGCLScript script = new EGCLScript(Main.class.getResource("/properties.egcl").getFile().toString());
 			try {
-				File file_java = new File(path + "src/main/java/monitoredfits/rv/Verification.java");
+				File file_java = new File(args[1] + "/generated-sources/java/Verification.java");
 				;
 
 				BufferedWriter output = new BufferedWriter(new FileWriter(file_java));
 				output.write(script.getVerificationCode());
 				output.close();
 
-				File file_aj = new File(path + "src/main/java/monitoredfits/rv/Properties.aj");
+				File file_aj = new File(args[1] + "/generated-sources/java/Properties.aj");
 				output = new BufferedWriter(new FileWriter(file_aj));
 				output.write(script.toAspectJ());
 				output.close();
