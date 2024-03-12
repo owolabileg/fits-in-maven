b1ff5ec0b0054a0ff36462f6befaab59deea30a7
Owolabi Legunsen <legunsen@cornell.edu>
2024-03-12 14:35:52 -0400

    started adding the tests

---
+++ chapter10/src/test/java/monitoredfits/fits/BaseTest.java
@@ -0,0 +1,20 @@
+package monitoredfits.fits;
+
+import monitoredfits.rv.Verification;
+import org.junit.Before;
+
+public class BaseTest {
+    static TransactionSystem transactionsystem = new TransactionSystem();
+
+    FrontEnd frontend = transactionsystem.getFrontEnd();
+
+    String account_number, account_number1, account_number2, account_number3, account_number_receiver;
+    Integer uid, uid_receiver;
+    Integer sid, sid1, sid2, sid_receiver;
+
+    @Before
+    public void reset() {
+        transactionsystem.setup();
+        Verification.setupVerification();
+    }
+}
